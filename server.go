9799c4dc1d443c5e6e7e5b835c43d013e099d7a4
yosp313 <147242847+yosp313@users.noreply.github.com>
2024-04-04 20:46:47 +0200

    added sqlite integration and the create functionality

--- server.go
+++ server.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+var db = dbConfig()
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
 func main() {
+	db.AutoMigrate(&Product{})
+
 	router := http.NewServeMux()
 
 	router.HandleFunc("/", nameController)
 	router.HandleFunc("/ip", ipController)
+	router.HandleFunc("POST /product", createProductController)
 
 	server := http.Server{
 		Addr:    ":8080",
@@ -36,6 +51,21 @@ func ipController(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Your IP is: %s", ip)
 }
 
+func createProductController(w http.ResponseWriter, r *http.Request) {
+	var product Product
+
+	err := json.NewDecoder(r.Body).Decode(&product)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	createProductService(&product)
+
+	fmt.Fprintf(w, "The product have been created successfully!: %+v", product)
+}
+
 func ReadUserIP(r *http.Request) string {
 	IPAddress := r.Header.Get("X-Real-Ip")
 	if IPAddress == "" {
@@ -45,4 +75,26 @@ func ReadUserIP(r *http.Request) string {
 		IPAddress = r.RemoteAddr
 	}
 	return IPAddress
+}
+
+func dbConfig() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	fmt.Println("Connected to db")
+
+	return db
+}
+
+func createProductService(product *Product) {
+	db.Create(product)
+}
+
+func findProductByCodeService(product Product, productCode string) any {
+	result := db.First(&product, "code = ?", productCode)
+
+	return result
 }
