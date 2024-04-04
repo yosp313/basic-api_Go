a61ae4ea1473fe3bfd3cb87f089003fd25af20ca
yosp313 <147242847+yosp313@users.noreply.github.com>
2024-04-04 19:14:36 +0200

    added a route for reading the request ip

--- server.go
+++ server.go
@@ -9,6 +9,7 @@ func main() {
 	router := http.NewServeMux()
 
 	router.HandleFunc("/", nameController)
+	router.HandleFunc("/ip", ipController)
 
 	server := http.Server{
 		Addr:    ":8080",
@@ -16,6 +17,7 @@ func main() {
 	}
 
 	server.ListenAndServe()
+	fmt.Printf("Server is listening on %s", server.Addr)
 }
 
 func nameController(w http.ResponseWriter, r *http.Request) {
@@ -26,4 +28,21 @@ func nameController(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
+
+func ipController(w http.ResponseWriter, r *http.Request) {
+	var ip string = ReadUserIP(r)
+
+	fmt.Fprintf(w, "Your IP is: %s", ip)
+}
+
+func ReadUserIP(r *http.Request) string {
+	IPAddress := r.Header.Get("X-Real-Ip")
+	if IPAddress == "" {
+		IPAddress = r.Header.Get("X-Forwarded-For")
+	}
+	if IPAddress == "" {
+		IPAddress = r.RemoteAddr
+	}
+	return IPAddress
 }
