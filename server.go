fbd9a0d9b2ce6e66115ac2ae75a756600982285a
CactusJoe <yosp31@gmail.com>
2024-04-04 16:53:43 +0200

    you can put your name in the path and it will display

--- server.go
+++ server.go
@@ -1,12 +1,19 @@
 package main
 
 import(
+  "fmt"
   "net/http"  
 )
 
 func main(){
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-    w.Write([]byte("Hello, World!"))
+    
+    if r.URL.Path[1:] == "" || len(r.URL.Path[1:]) < 3 {
+			fmt.Fprintf(w, "Hello, Dumbass!")
+			return
+		}
+
+		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
   })
 
   http.ListenAndServe(":8080", nil)
