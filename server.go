d6975f118926e23b1a1790ebfd5c637084429ae7
yosp313 <147242847+yosp313@users.noreply.github.com>
2024-05-25 13:22:59 +0300

    Added nicer formatting

--- server.go
+++ server.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-	"basic-api/configs"
-	"basic-api/controllers"
-	"basic-api/middleware"
-	"basic-api/models"
-	"fmt"
-	"net/http"
+  "basic-api/configs"
+  "basic-api/controllers"
+  "basic-api/middleware"
+  "basic-api/models"
+  "fmt"
+  "net/http"
 
-	"gorm.io/gorm"
+  "gorm.io/gorm"
 )
 
 var db *gorm.DB = configs.DbConfig()
 
 func main() {
-	db.AutoMigrate(&models.Product{})
+  db.AutoMigrate(&models.Product{})
 
   router := http.NewServeMux()
 
-	router.HandleFunc("/", controllers.NameController)
-	router.HandleFunc("/ip", controllers.IpController)
-	router.HandleFunc("POST /product", controllers.CreateProductController)
-	router.HandleFunc("GET /product", controllers.ListAllProductsController)
-	router.HandleFunc("GET /product/{id}", controllers.FindProductsController)
-	router.HandleFunc("PUT /product/{id}", controllers.UpdateProductController)
-	router.HandleFunc("DELETE /product/{id}", controllers.DeleteProductController)
+  router.HandleFunc("/", controllers.NameController)
+  router.HandleFunc("/ip", controllers.IpController)
+  router.HandleFunc("POST /product", controllers.CreateProductController)
+  router.HandleFunc("GET /product", controllers.ListAllProductsController)
+  router.HandleFunc("GET /product/{id}", controllers.FindProductsController)
+  router.HandleFunc("PUT /product/{id}", controllers.UpdateProductController)
+  router.HandleFunc("DELETE /product/{id}", controllers.DeleteProductController)
 
-	server := http.Server{
-		Addr:    ":8080",
-		Handler: middleware.Logging(router),
-	}
+  server := http.Server{
+    Addr:    ":8080",
+    Handler: middleware.Logging(router),
+  }
 
-	server.ListenAndServe()
-	fmt.Printf("Server is listening on %s", server.Addr)
+  server.ListenAndServe()
+  fmt.Printf("Server is listening on %s", server.Addr)
 }
